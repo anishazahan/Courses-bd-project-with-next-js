9aa46fd7dd4fdbbee8eaa5dbe26adb2d64993103
anishazahan <anishazahan13@gmail.com>
2024-07-01 04:48:01 +0600

    populate courses

--- app/page.js
+++ app/page.js
@@ -1,6 +1,9 @@
+import { getCourses } from "@/BackendService/queries/courses";
 import Test from "@/components/Test";
 
-export default function Home() {
+export default async function Home() {
+  const data = await getCourses();
+  console.log(data);
   return (
     <>
       ....,,,,,
