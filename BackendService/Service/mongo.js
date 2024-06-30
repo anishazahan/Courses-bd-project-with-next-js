5c995fdf18df76db7b774348ff55b525b9aefb0d
anishazahan <anishazahan13@gmail.com>
2024-07-01 04:19:51 +0600

    database connected

---
+++ BackendService/Service/mongo.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+export async function dbConnect() {
+  try {
+    const connection = await mongoose.connect(
+      String(process.env.MONGODB_CONNECTION_STRING)
+    );
+
+    return connection;
+  } catch (err) {
+    console.error(err);
+  }
+}
